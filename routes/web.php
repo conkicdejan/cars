acdb020f9f76e59d0e3ee0692c616e2750e3077f
Dejan Conkic <conkicdejan@gmail.com>
2022-05-20 18:20:16 +0200

    Task no.3

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Dejan';
+    $age = 40;
+    return view('welcome', compact('name', 'age'));
 });
 
 Route::get('/about', function () {
